5df366643334b1327dc10e1f690e00968284d918
Web4 <167559384+Web4application@users.noreply.github.com>
2025-04-21 19:23:45 -0700

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -1,6 +1,8 @@
 git init
 git remote add origin https://github.com/Web4application/Lola.git
+git remote add origin https://github.com/Web4application/Lola-ai.git
 git add .
 git commit -m "Initial Lola commit"
 git branch -M main
 git push -u origin main
+
