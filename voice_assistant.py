f034fffd8c3088acded3de9ec0acada26e54110f
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-16 00:38:00 +0200

    Create voice_assistant.py

---
+++ voice_assistant.py
@@ -0,0 +1,8 @@
+from lola_voice import record_audio, transcribe_audio, speak_text
+
+# Replace input() with:
+record_audio()
+user_input = transcribe_audio()
+
+# Replace print(Lola's reply) with:
+speak_text(lola_reply)
