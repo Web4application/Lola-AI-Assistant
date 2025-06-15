55b1b3794e2db17273173394251a39fee5fd38de
Web4 <167559384+Web4application@users.noreply.github.com>
2025-06-16 00:55:56 +0200

    Delete voice_assistant.py

--- voice_assistant.py
+++
@@ -1,8 +0,0 @@
-from lola_voice import record_audio, transcribe_audio, speak_text
-
-# Replace input() with:
-record_audio()
-user_input = transcribe_audio()
-
-# Replace print(Lola's reply) with:
-speak_text(lola_reply)
